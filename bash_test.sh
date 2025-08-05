4442a7efc2e3ca42a67202bb728b2e32e902f692
AdamSmith-physics <adam.smith.home@ntlworld.com>
2025-08-05 16:46:56 +0100

    Update local script

--- bash_test.sh
+++ bash_test.sh
@@ -5,8 +5,18 @@ if [ ! -d "logs" ]; then
     mkdir logs
 fi
 
+dt=0.25
+p=0.25
+Nx=4
+Ny=4
+V=1.0
+b=0.1
+num_iterations=500
+steps=100
+fermions=false 
+
 max=15
 for i in `seq 1 $max`
 do
-    nohup julia --project=. run/run_trajectories.jl $i > logs/log_$i.out &
+    nohup julia --project=. run/run_trajectories.jl $i $dt $p $Nx $Ny $V $b $num_iterations $steps $fermions > logs/log_$i.out &
 done
